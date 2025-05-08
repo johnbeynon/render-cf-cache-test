b4f45ea357df9fecfa6aaff4c1f00eb242882c2b
John Beynon <john@render.com>
2025-05-08 09:50:10 +0100

    Read and parse the body

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 # myapp.rb
 require 'sinatra'
 
+before do
+  if request.body.size > 0
+    request.body.rewind
+    @params = ActiveSupport::JSON.decode(request.body.read)
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -14,6 +21,6 @@ post '/ping' do
 end
 
 post '/template/:id' do
-  puts params.inspect
+  puts @params.inspect
   "This is a template for #{params["name"]}"
 end
